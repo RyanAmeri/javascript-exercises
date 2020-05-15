2bb1fe9de648b45de93b2101674db111d16a6dd9
Ryan Ameri <ryan@internettradie.com.au>
2020-05-15 21:29:36 +1000

    finished repeatString

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let endText = '';
+    if (num < 0){
+        endText = 'ERROR';
+    }
+    else{
+        for (let i = 0; i < num; i++){
+            endText += text;
+        }
+    } 
+    return endText;
 }
 
 module.exports = repeatString
