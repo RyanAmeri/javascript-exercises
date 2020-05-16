1bb44e5ca0cca8c97913e1e1826d218efe8bac29
Ryan Ameri <ryan@internettradie.com.au>
2020-05-17 06:04:08 +1000

    finished temp conversion

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,14 @@
-const ftoc = function() {
+const ftoc = function(fah) {
+  let cel = ((fah - 32) * (5/9));
+  cel = Math.round((cel + Number.EPSILON) * 10) / 10;
+  return cel;
 
 }
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let fah = (cel * (9/5) + 32);
+  fah = Math.round((fah + Number.EPSILON) * 10) / 10;
+  return fah;
 }
 
 module.exports = {
