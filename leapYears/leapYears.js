35a040b5181073d33107b8a11d791aa1b294b287
Ryan Ameri <ryan@internettradie.com.au>
2020-05-17 05:52:01 +1000

    finished leapYear

--- leapYears/leapYears.js
+++ leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let leap = false;
+    if (year % 4 === 0){
+        leap = true;
+        if ((year % 100 === 0) && (year % 400 !== 0))
+            leap = false;
+    }
+return leap;
 }
-
+//console.log(leapYears(2000));
 module.exports = leapYears
