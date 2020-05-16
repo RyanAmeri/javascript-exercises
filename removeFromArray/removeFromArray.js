8a28d2e909120f56d0d23163fe5f052474e0899c
Ryan Ameri <ryan@internettradie.com.au>
2020-05-16 20:40:22 +1000

    Oh my god I learned so much doing remove from array. Rest parameters, spread operator... etc etc

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(origArray,...removeArray) {
+    let newArray = [...origArray]; 
 
+    for (let origItem = 0; origItem < origArray.length ; origItem++){
+        for (let removeItem = 0; removeItem < removeArray.length ; removeItem++){
+            if (origArray[origItem] === removeArray[removeItem]){
+                newArray.splice(newArray.indexOf(origArray[origItem]),1);
+            }
+               
+        }
+    }
+    return newArray;
 }
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
