b6a982cb225c129ae449e2fd7328c6be98a05edd
Ryan Ameri <ryan@internettradie.com.au>
2020-05-21 22:48:46 +1000

    finished caesar crypto function

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,29 +1,36 @@
 const caesar = function(sentence, num) {
-    let lAlph = /[a-z]/;
-    let uAlph = /[A-Z]/;
-    //console.log(lAlph.test("AA"));
+    let lAlph = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p',
+                'q', 'r','s','t','u','v','w','x','y','z'];
+    let uAlph = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
+    'Q', 'R','S','T','U','V','W','X','Y','Z'];
     let newSentence = [];
     [...sentence].map( c => {
-        if (lAlph.test(c) || uAlph.test(c)) {
-            console.log(c);
-            c = c.charCodeAt(c) + num;
-            c = String.fromCharCode(c);
-            console.log(c);
-            newSentence.push(c);
+        let i = lAlph.indexOf(c);
+        let I = uAlph.indexOf(c);
+        if ( i !== -1) {
+            newSentence.push(lAlph[modulo((i + num),26)]);
+        }
+        else if ( I !== -1) {
+            newSentence.push(uAlph[modulo((I + num),26)]);
         }
         else {
             newSentence.push(c);
         }
-        
         return c;
     
     });
     
-    newSentence = newSentence.toString();
-    const searchRegExp = /,/g;
-    newSentence = newSentence.replace(searchRegExp,'');
-    console.log(newSentence);
+    //ewSentence = newSentence.toString();
+    //const searchRegExp = /,/g;
+    newSentence = newSentence.join('');
+    return newSentence;
 }
 
-console.log(caesar("aaa BBB ZZZ 111", 1));
-//module.exports = caesar
+function modulo (x, m) {
+ 
+    return (x % m + m) % m;
+ 
+}
+
+//console.log(caesar("aaa bbb ccc AAA BBB CCC", -1));
+module.exports = caesar
