981191b535237eb80d21fdad57b339702423ab4b
Ryan Ameri <ryan@internettradie.com.au>
2020-05-21 17:10:01 +1000

    implemented caesar function but used unicode characters so it doesn't loop according to spec, needs a rework

--- caesar/caesar.js
+++ caesar/caesar.js
@@ -1,5 +1,29 @@
-const caesar = function() {
-
+const caesar = function(sentence, num) {
+    let lAlph = /[a-z]/;
+    let uAlph = /[A-Z]/;
+    //console.log(lAlph.test("AA"));
+    let newSentence = [];
+    [...sentence].map( c => {
+        if (lAlph.test(c) || uAlph.test(c)) {
+            console.log(c);
+            c = c.charCodeAt(c) + num;
+            c = String.fromCharCode(c);
+            console.log(c);
+            newSentence.push(c);
+        }
+        else {
+            newSentence.push(c);
+        }
+        
+        return c;
+    
+    });
+    
+    newSentence = newSentence.toString();
+    const searchRegExp = /,/g;
+    newSentence = newSentence.replace(searchRegExp,'');
+    console.log(newSentence);
 }
 
-module.exports = caesar
+console.log(caesar("aaa BBB ZZZ 111", 1));
+//module.exports = caesar
