12a30b9bd7492593768123e709d6b697d1113307
Ryan Ameri <ryan@internettradie.com.au>
2020-05-21 13:14:08 +1000

    implemented palindrome

--- palindromes/palindromes.js
+++ palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function() {
-
+const palindromes = function(sentence) {
+    let lowerSentence = sentence.toLowerCase();
+    let arr = [...lowerSentence];
+    let clean = arr.filter( (letter) => {
+        return letter !== " " && letter !== ',' && letter !== '.' && letter !== '!';
+    });
+    
+    arr2 = [...clean];
+    arr2.reverse();
+    let condition = true;
+    for (let i = 0; i < clean.length; i++){
+        if (clean[i] !== arr2[i])
+        condition = false;
+    }
+    return condition;
 }
 
+//console.log(palindromes("Racecar"));
 module.exports = palindromes
