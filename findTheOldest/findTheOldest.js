a26bc108ca42e97237594da43d7bcffc8fd3f24c
Ryan Ameri <ryan@internettradie.com.au>
2020-05-22 12:01:40 +1000

    implemented oldest person

--- findTheOldest/findTheOldest.js
+++ findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-let findTheOldest = function() {
-
+let findTheOldest = function(people) {
+    let oldest = people.reduce((oldestPerson, person) => {
+        let currentYear = new Date();
+        currentYear = currentYear.getFullYear();
+        let oldestAge;
+        let age;
+        (person.yearOfDeath) ? 
+            age = person.yearOfDeath - person.yearOfBirth : age = currentYear - person.yearOfBirth;
+        (oldestPerson.yearOfDeath) ? 
+            oldestAge = oldestPerson.yearOfDeath - oldestPerson.yearOfBirth : oldestAge = currentYear - oldestPerson.yearOfBirth; 
+        console.log(person, age, oldestPerson)
+        
+        if(age > oldestAge){
+            oldestPerson = person;
+        }
+        return oldestPerson;
+        
+    }, people[0]);    
+    return oldest;
 }
 
 module.exports = findTheOldest
